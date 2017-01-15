61788a6f3cda4ccbea7426a82f905e76d301db42
Joseph Nguyen <josephnguyen@josephs-mbp>
2017-01-14 16:41:24 -0800

    Initial Commit

---
+++ To Do App!/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  To Do App!
+//
+//  Created by Joseph Nguyen on 1/14/17.
+//  Copyright © 2017 Joseph Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
